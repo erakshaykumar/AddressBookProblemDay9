3bcaab75feaf05cc3bceb67a1d2602a68055d9e5
erakshaykumar <akaakshay304@gmail.com>
2022-02-23 02:01:17 +0530

    Added: Add Address Book System Class In Address Book

--- src/com/bridgelabz/addressbooksystems/AddressBookSystem.java
+++ src/com/bridgelabz/addressbooksystems/AddressBookSystem.java
@@ -1,103 +1,48 @@
 package com.bridgelabz.addressbooksystems;
 
-class Contact {
-	private String firstName;
-	private String lastName;
-	private String address;
-	private String city;
-	private int zipcode;
-	private String state;
-	private String phoneNo;
-	private String email;
-
-	public Contact(String firstName, String lastName, String address, String city, int zipcode, String state,
-			String phoneNo, String email) {
-		setFirstName(firstName);
-		setLastName(lastName);
-		setAddress(address);
-		setCity(city);
-		setState(state);
-		setZipcode(zipcode);
-		setPhoneNo(phoneNo);
-		setEmail(email);
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public int getZipcode() {
-		return zipcode;
-	}
-
-	public void setZipcode(int zipcode) {
-		this.zipcode = zipcode;
-	}
-
-	public String getState() {
-		return state;
-	}
-
-	public void setState(String state) {
-		this.state = state;
-	}
-
-	public String getPhoneNo() {
-		return phoneNo;
-	}
-
-	public void setPhoneNo(String phoneNo) {
-		this.phoneNo = phoneNo;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
-				+ ", zipcode=" + zipcode + ", state=" + state + ", phoneNo=" + phoneNo + ", email=" + email + "]";
-	}
-
-}
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookSystem {
-	public static void main(String[] args) {
-		Contact contact = new Contact("Akshay", "Kumar", "Celestial City", "Pune", 412101, "Maharashtra", "7057082582",
-				"akaakshay304@gmail.com");
+	static Scanner scanner = new Scanner(System.in);
+	ArrayList<Contacts> person = new ArrayList<>();
+
+	// add new person record to array list after taking input
+
+	public void addPerson() {
+		System.out.println("Enter the First Name");
+		String firstName = scanner.next();
+		System.out.println("Enter the Last Name");
+		String lastName = scanner.next();
+		System.out.println("Enter the Address");
+		String address = scanner.next();
+		System.out.println("Enter the City");
+		String city = scanner.next();
+		System.out.println("Enter the State");
+		String state = scanner.next();
+		System.out.println("Enter the Zip");
+		int zipcode = scanner.nextInt();
+		System.out.println("Enter the PhoneNumber");
+		String phoneNo = scanner.next();
+		System.out.println("Enter the Email");
+		String email = scanner.next();
+
+		// creating constructor
+		Contacts contact = new Contacts(firstName, lastName, address, city, zipcode, state, phoneNo, email);
+
+		// add the above list to to Contacts array list
+		person.add(contact);
+
+		// printing contacts object data inside
 		System.out.println(contact);
+
 	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+		AddressBookSystem address = new AddressBookSystem();
+		address.addPerson();
+
+	}
+
 }
